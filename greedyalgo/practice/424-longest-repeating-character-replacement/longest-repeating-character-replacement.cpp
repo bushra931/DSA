9e26742c33d9415d88f0dbd054d7ab65f3a135a1
Bushra khan <141857170+bushra931@users.noreply.github.com>
2025-03-18 14:06:57 +0530

    Time: 9 ms (39.74%) | Memory: 11 MB (43.52%) - LeetSync

---
+++ greedyalgo/practice/424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l=0,r=0,maxlen=0,maxf=0;
+        unordered_map<int,int> freq;
+        while(r<s.size()){
+            freq[s[r]]++;
+            maxf=max(maxf,freq[s[r]]);
+            while((r-l+1)-maxf>k){
+                freq[s[l]]--;
+              
+                l++;
+            }
+            if((r-l+1)-maxf<=k){
+                maxlen=max(maxlen,r-l+1);
+            }
+            r++;
+
+
+        }
+        return maxlen;
+        
+    }
+};
