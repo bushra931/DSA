27694f2d9cb06a3b6c72777491b91515f912c561
Bushra khan <141857170+bushra931@users.noreply.github.com>
2025-03-29 22:36:05 +0530

    Time: 79 ms (78.88%) | Memory: 44.1 MB (84.96%) - LeetSync

--- graph/practice/744-network-delay-time/network-delay-time.cpp
+++ graph/practice/744-network-delay-time/network-delay-time.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n + 1);
-        for (auto &time : times) {
-            adj[time[0]].push_back({time[1], time[2]});
+         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(auto &time : times){
+            adj[time[0]].push_back({time[1],time[2]});
         }
-
-        // Ordered set {time, node}
-        set<pair<int, int>> s;
-        
-        vector<int> t(n + 1, INT_MAX);
-        t[k] = 0;
-        s.insert({0, k});
-
-        while (!s.empty()) {
-            auto [time, node] = *s.begin();
-            s.erase(s.begin());
-
-            // Process all neighbors
-            for (auto &[tnode, newt] : adj[node]) {
-                if (time + newt < t[tnode]) {
-                    s.erase({t[tnode], tnode});  // Remove old value if present
-                    t[tnode] = time + newt;
-                    s.insert({t[tnode], tnode});
+        vector<int> t(n+1,1e9);
+        t[k]=0;
+        q.push({0,k});
+        while(!q.empty()){
+            auto it=q.top();
+            int time=it.first;
+            int node =it.second;
+            q.pop();
+            for(auto neigh: adj[node]){
+                int newt=neigh.second;
+                int tnode=neigh.first;
+                if(time+newt<t[tnode]){
+                    t[tnode]=time+newt;
+                    q.push({time+newt,tnode});
                 }
+
             }
         }
+        int mini=0;
+        for(int i=1;i<=n;i++){
+            if(t[i]==1e9) return -1;
+            mini=max(mini,t[i]);
 
-        // Find the maximum time to reach any node
-        int maxTime = 0;
-        for (int i = 1; i <= n; i++) {
-            if (t[i] == INT_MAX) return -1;  // Some nodes are unreachable
-            maxTime = max(maxTime, t[i]);
         }
-
-        return maxTime;
+        return mini;
 
 
         
