6e27a6ab589e8bfd2f654418eacb0f6f6d47fd74
Bushra khan <141857170+bushra931@users.noreply.github.com>
2025-04-20 12:59:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.80%) - LeetSync

--- graph/practice/198-house-robber/house-robber.cpp
+++ graph/practice/198-house-robber/house-robber.cpp
@@ -1,25 +1,18 @@
 class Solution {
-private:
-    int solve(int n,vector<int>& arr,vector<int> &dp){
-       if (n == 0) return 0;
-    if (n == 1) return arr[0];
-
-    dp[0] = arr[0];
-    dp[1] = max(arr[0], arr[1]);
-
-    for (int i = 2; i < n; i++) {
-        int pick = arr[i] + dp[i - 2];
-        int nonpick = dp[i - 1];
-        dp[i] = max(pick, nonpick);
-    }
-
-    return dp[n - 1];
-    }
-
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return solve(n,nums,dp);   
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=0;i<n;i++){
+            int pick=nums[i];
+            if(i>1) pick+=prev2;
+            int nonpick=0+prev;
+            int curr_i=max(pick,nonpick);
+            prev2=prev;
+            prev=curr_i;
+
+        }
+        return prev;
     }
 };
