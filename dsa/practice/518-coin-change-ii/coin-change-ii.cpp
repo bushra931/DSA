5e84f368bdc954c439cdb2c48b97410994bbec9f
Bushra khan <141857170+bushra931@users.noreply.github.com>
2025-06-06 16:09:23 +0530

    Time: 11 ms (79.10%) | Memory: 10.7 MB (75.84%) - LeetSync

--- dsa/practice/518-coin-change-ii/coin-change-ii.cpp
+++ dsa/practice/518-coin-change-ii/coin-change-ii.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<unsigned long long> dp(amount + 1, 0); 
-        dp[0] = 1;
+        vector<unsigned long long> prev(amount+1,0), curr(amount+1,0);
         
-        for (int c : coins) {
-            for (int a = c; a <= amount; a++) {
-                dp[a] += dp[a - c];
+        prev[0] = 1;
+        
+        for(int ind=0; ind<coins.size(); ind++){
+            curr[0] = 1;
+            for(int T=1; T<=amount; T++){
+                unsigned long long nottake = prev[T];
+                unsigned long long take = 0;
+                if(coins[ind] <= T){
+                    take = curr[T - coins[ind]];
+                }
+                curr[T] = take + nottake;
             }
+            prev = curr;
         }
         
-        return dp[amount];        
+        return prev[amount];
     }
 };
