fc06c9607b8109f43b902ede85b2e12460ac8bb7
Bushra khan <141857170+bushra931@users.noreply.github.com>
2025-06-09 13:05:44 +0530

    Time: 145 ms (57.09%) | Memory: 139.8 MB (53.44%) - LeetSync

--- dsa/practice/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ dsa/practice/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -13,8 +13,20 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(4,-1));
-        return f(0,0,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int>(5,0));
 
+        for(int ind=n-1;ind>=0;ind--){
+            for(int trans=3;trans>=0;trans--){
+                 if(trans%2==0){
+                    dp[ind][trans]=max(-prices[ind]+dp[ind+1][trans+1],0+dp[ind+1][trans]);
+            }
+                else{
+                    dp[ind][trans]=max(prices[ind]+dp[ind+1][trans+1],0+dp[ind+1][trans]);
+                }
+            }
+        }
+        return dp[0][0];
+
+    
     }
 };
