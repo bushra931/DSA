acb3bba1dcdfa880b7cf281345936edbcbef204b
Bushra khan <141857170+bushra931@users.noreply.github.com>
2025-06-09 17:10:13 +0530

    Time: 7 ms (75.55%) | Memory: 58.8 MB (97.14%) - LeetSync

--- dsa/practice/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ dsa/practice/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int f(int ind, int buy,vector<int>& prices,vector<vector<int>>& dp,int fee){
-        //base case
-        if(ind >= prices.size()) return 0;
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
-        if(buy){
-            return dp[ind][buy]=max(-prices[ind]+f(ind+1,0,prices,dp,fee),0+f(ind+1,1,prices,dp,fee));
-        }
-        else{
-            //sell
-            return dp[ind][buy]=max(prices[ind]-fee+f(ind+1,1,prices,dp,fee),0+f(ind+1,0,prices,dp,fee));
-        }
-    }
+    // int f(int ind, int buy,vector<int>& prices,vector<vector<int>>& dp,int fee){
+    //     //base case
+    //     if(ind >= prices.size()) return 0;
+    //     if(dp[ind][buy]!=-1) return dp[ind][buy];
+    //     if(buy){
+    //         return dp[ind][buy]=max(-prices[ind]+f(ind+1,0,prices,dp,fee),0+f(ind+1,1,prices,dp,fee));
+    //     }
+    //     else{
+    //         //sell
+    //         return dp[ind][buy]=max(prices[ind]-fee+f(ind+1,1,prices,dp,fee),0+f(ind+1,0,prices,dp,fee));
+    //     }
+    // }
     int maxProfit(vector<int>& prices, int fee) {
-         vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
-         return f(0,1,prices,dp,fee);
-        
+        int n=prices.size();
+        int aheadnotbuy,aheadbuy,currbuy,currnotbuy;
+        aheadnotbuy=aheadbuy=0;
+        for(int ind=n-1;ind>=0;ind--){
+            currnotbuy=max(prices[ind]+aheadbuy,0+aheadnotbuy);
+            currbuy=max(-prices[ind]-fee+aheadnotbuy,0+aheadbuy);
+            aheadbuy=currbuy;
+            aheadnotbuy=currnotbuy;
+        }
+        return aheadbuy;
     }
 };
