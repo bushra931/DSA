f613fe13b259ffe6833a4837ee543ef10e5da817
Bushra khan <141857170+bushra931@users.noreply.github.com>
2025-06-13 12:30:47 +0530

    Time: 1512 ms (39.65%) | Memory: 9.2 MB (93.75%) - LeetSync

--- dsa/practice/132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ dsa/practice/132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -7,23 +7,36 @@ public:
         }
         return true;
     }
-    int f(int i,string& s,vector<int>& dp){
-        int n=s.size();
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int mincost=INT_MAX;
-        for(int j=i;j<n;j++){
-            if(ispalindrome(i,j,s)){
-                int cost=1+f(j+1,s,dp);
-                mincost=min(mincost,cost);
-            }
-        }
-        return dp[i]=mincost;
+    // int f(int i,string& s,vector<int>& dp){
+    //     int n=s.size();
+    //     if(i==n) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int mincost=INT_MAX;
+    //     for(int j=i;j<n;j++){
+    //         if(ispalindrome(i,j,s)){
+    //             int cost=1+f(j+1,s,dp);
+    //             mincost=min(mincost,cost);
+    //         }
+    //     }
+    //     return dp[i]=mincost;
 
-    }
+    // }
     int minCut(string& s) {
-        vector<int> dp(s.size(),-1);
-        return f(0,s,dp)-1;
+        int n=s.size();
+        vector<int> dp(n+1,-1);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int mincost=INT_MAX;
+            for(int j=i;j<n;j++){
+                if(ispalindrome(i,j,s)){
+                    int cost=1+dp[j+1];
+                    mincost=min(mincost,cost);
+                }
+            }
+           dp[i]=mincost;
+
+        }
+        return dp[0]-1; 
         
     }
 };
