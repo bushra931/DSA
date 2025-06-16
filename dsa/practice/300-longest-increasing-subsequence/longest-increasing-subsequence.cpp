3c7d4bb7f7c34c51efffe52623c5d9379a74b600
Bushra khan <141857170+bushra931@users.noreply.github.com>
2025-06-16 09:30:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (87.38%) - LeetSync

--- dsa/practice/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ dsa/practice/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    // int f(int ind,int prev_ind,vector<int>& nums,vector<vector<int>>& dp){
-    //     if(ind==nums.size()) return 0;
-    //     if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
-    //     //not take
-    //     int len=0+f(ind+1,prev_ind,nums,dp);
-    //     //take
-    //     if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
-    //         len=max(len,1+f(ind+1,ind,nums,dp));
-    //     }
-    //     return dp[ind][prev_ind+1]=len;
-    // }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for(int ind=n-1;ind>=0;ind--){
-            for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--){
-                int len=0+dp[ind+1][prev_ind+1];
-                if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
-                    len=max(len,1+dp[ind+1][ind+1]);
-                }
-                dp[ind][prev_ind+1]=len;
-
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+            }
+            else{
+                int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[ind]=nums[i];
             }
         }
-        return dp[0][0];
-
-        
+        return temp.size();
     }
 };
