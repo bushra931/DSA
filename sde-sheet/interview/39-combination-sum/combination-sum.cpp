6226fded44483d9a161fc571c5051d296069eab2
Bushra khan <141857170+bushra931@users.noreply.github.com>
2025-07-02 21:53:31 +0530

    Time: 3 ms (56.51%) | Memory: 14 MB (90.88%) - LeetSync

---
+++ sde-sheet/interview/39-combination-sum/combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void findcombo(int ind,int target,vector<int>& arr,vector<vector<int>>& ans,vector<int>& ds){
+        if(ind==arr.size()){
+            if(target==0) ans.push_back(ds);
+            return;
+        }
+        if(arr[ind]<=target){
+            ds.push_back(arr[ind]);
+            findcombo(ind,target-arr[ind],arr,ans,ds);
+            ds.pop_back();
+        }
+        findcombo(ind+1,target,arr,ans,ds);
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> ds;
+        vector<vector<int>> ans;
+        findcombo(0,target,candidates,ans,ds);
+        return ans;
+
+    }
+};
