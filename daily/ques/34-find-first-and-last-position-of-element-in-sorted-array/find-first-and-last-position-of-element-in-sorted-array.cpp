cb269d51cd06ad1ff971a6654420215289ef0b61
Bushra khan <141857170+bushra931@users.noreply.github.com>
2026-01-02 14:50:10 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (85.53%) - LeetSync

--- daily/ques/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ daily/ques/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int s=0;
-        int e=nums.size()-1;
-        int mid=s+(e-s)/2;
-        int ans_s=-1;
-        int ans_e=-1;
-        while(s<=e){
-            if(nums[mid]==target){
-                ans_s=mid;
-                while(ans_s>=0 && nums[ans_s]==target) ans_s--;
-                ans_e=mid;
-                while(ans_e<nums.size() &&nums[ans_e]==target) ans_e++;
-                return {ans_s+1,ans_e-1};
+    int firstpos(vector<int>& nums, int target){
+        int left=0;int right=nums.size()-1;
+        int ans=-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>=target){
+                if(nums[mid]==target) ans=mid;
+                right=mid-1;
             }
-            else if(nums[mid]<target){
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
-            mid=s+(e-s)/2;
+            else left=mid+1;
         }
-        return {ans_s,ans_e};
-        
+        return ans;
+    }
+    int lastpos(vector<int>& nums, int target){
+        int left=0;int right=nums.size()-1;
+        int ans=-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]<=target){
+                if(nums[mid]==target) ans=mid;
+                left=mid+1;
+            }
+            else right=mid-1;
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+          int start=firstpos(nums,target);
+          if(start==-1) return {-1,-1};
+          int end=lastpos(nums,target);
+          return {start,end};
     }
 };
