a783b4b721db9b6c6e744bc66e521e8aff77709b
Bushra khan <141857170+bushra931@users.noreply.github.com>
2026-07-17 13:01:49 +0530

    Time: 14 ms (83.77%) | Memory: 66.5 MB (97.38%) - LeetSync

---
+++ daily/ques/892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        deque<int> dq;
+        int n=nums.size();
+        vector<int> prefixsum(n,0);
+        int res=INT_MAX;
+        int j=0;
+        while(j<n){
+            if(j==0) prefixsum[j]= nums[j];
+            else prefixsum[j]=prefixsum[j-1]+nums[j];
+            if(prefixsum[j]>=k) res=min(res,j+1);
+            //shrink window or not 
+            while(!dq.empty() && prefixsum[j]-prefixsum[dq.front()]>=k){
+                res=min(res,j-dq.front());
+                dq.pop_front();
+            }
+            // dip agya case --> so to store inc only 
+            while(!dq.empty() && prefixsum[j]<=prefixsum[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(j);
+            j++;
+        }
+       return res == INT_MAX ? -1 : res;
+
+        
+    }
+};
