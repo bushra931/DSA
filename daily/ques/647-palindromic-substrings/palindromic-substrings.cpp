42d092319736d92d3efe4e45e0639d3fa7d0d583
Bushra khan <141857170+bushra931@users.noreply.github.com>
2026-07-29 16:04:14 +0530

    Time: 52 ms (20.02%) | Memory: 59.4 MB (11.48%) - LeetSync

--- daily/ques/647-palindromic-substrings/palindromic-substrings.cpp
+++ daily/ques/647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-
         int n = s.size();
-        int count = 0;
 
-        for(int center = 0; center < n; center++) {
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<vector<int>> dp1(n, vector<int>(n, 0));
 
-            int left = center, right = center;
+        // Length = 1
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+            dp1[i][i] = 1;
+        }
 
-            while(left >= 0 && right < n && s[left] == s[right]) {
-                count++;
-                left--;
-                right++;
-            }
+        // Length = 2
+        for (int i = 0; i + 1 < n; i++) {
+            if (s[i] == s[i + 1])
+                dp[i][i + 1] = 1;
 
-            left = center;
-            right = center + 1;
+            dp1[i][i + 1] = dp1[i][i] + dp1[i + 1][i + 1] + dp[i][i + 1];
+        }
 
-            while(left >= 0 && right < n && s[left] == s[right]) {
-                count++;
-                left--;
-                right++;
+        // Length >= 3
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+
+                if (s[i] == s[j] && dp[i + 1][j - 1])
+                    dp[i][j] = 1;
+
+                dp1[i][j] = dp1[i][j - 1]
+                          + dp1[i + 1][j]
+                          - dp1[i + 1][j - 1]
+                          + dp[i][j];
             }
         }
 
-        return count;
+        return dp1[0][n - 1];
     }
-        
 };
